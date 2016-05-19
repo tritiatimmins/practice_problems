288a123f8bfcdcee0b5f0b219ee83dab79659244
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-05-19 14:27:25 -0700

    trimming those edges!

--- replaceSpace.js
+++ replaceSpace.js
@@ -10,3 +10,4 @@ var replaceSpace = function(string) {
 console.log(replaceSpace("string thing ")); //'string%20thing'
 console.log(replaceSpace("hello it's me")); //hello%20it's%20me%20%20%20%20 want --> 'hello%20it's%20me'
 console.log(replaceSpace("    hey gurl   "));
+console.log(replaceSpace("   hey  gurl  "));
